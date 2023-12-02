8c33d25fdd4ad98dbc25dff2a549f486ba2776c3
Youbeen Choi <youbs26@konkuk.ac.kr>
2023-12-02 17:33:15 +0900

    fix: homeFragment, pushActivity design

--- app/src/main/java/com/example/tuesday/MainActivity.kt
+++ app/src/main/java/com/example/tuesday/MainActivity.kt
@@ -7,11 +7,10 @@ import com.example.tuesday.calendar.ScheduleModel
 import com.example.tuesday.calendar.UtilListener
 import com.example.tuesday.calendar.UtilObject
 import com.example.tuesday.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         UtilObject.currentActivity = this
@@ -26,12 +25,22 @@ class MainActivity : AppCompatActivity() {
             binding.viewPager.currentItem = 1
         }
 
-        binding.mainFragment.setOnClickListener {
-            binding.viewPager.currentItem = 0
+        val bottomNavigation = binding.bottomNavigation
+        bottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_home ->  binding.viewPager.currentItem = 0
+                R.id.menu_calendar -> binding.viewPager.currentItem = 1
+//                R.id.menu_bouquet -> binding.vew
+            }
+            true
         }
 
-        binding.calendarFragment.setOnClickListener {
-            binding.viewPager.currentItem = 1
-        }
+//        binding.mainFragment.setOnClickListener {
+//            binding.viewPager.currentItem = 0
+//        }
+//
+//        binding.calendarFragment.setOnClickListener {
+//            binding.viewPager.currentItem = 1
+//        }
     }
 }
